0baab29b744a5db2e16fe8ee281c9bf73a06c9a3
Hannes Bornman <Hannes@explore-datascience.net>
2019-09-25 14:26:41 +0200

    Added a filter

---
+++ Hannes/test.py
@@ -0,0 +1,133 @@
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+from dash.dependencies import Input, Output
+import pandas as pd
+import dash_table
+
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+
+sql_statement = r"""WITH CTE as (
+      SELECT DISTINCT DATE(ORDER_CREATION_DATE) ORDER_CREATION_DATE,
+      ORDER_ID_ANON,
+      ACTION_TYPE_DESC
+       FROM `bcx-insights.telkom_customerexperience.orders_20190903_00_anon`
+       WHERE ACCOUNT_NO_ANON = -4302568719576920909
+      )
+      SELECT *, ROW_NUMBER() OVER (PARTITION BY ORDER_ID_ANON ORDER BY ORDER_CREATION_DATE) Stage
+      FROM CTE
+      order by ORDER_ID_ANON, ORDER_CREATION_DATE DESC"""
+
+
+def read_df(sql):
+    return pd.io.gbq.read_gbq(sql, project_id='bcx-insights', dialect='standard')
+    
+
+df = read_df(sql_statement)
+
+
+
+app.layout = html.Div(
+    className="row",
+    children=[
+        html.Div(
+            dash_table.DataTable(
+                id='table-paging-with-graph',
+                columns=[
+                    {"name": i, "id": i} for i in sorted(df.columns)
+                ],
+                page_current=0,
+                page_size=20,
+                page_action='custom',
+
+                filter_action='custom',
+                filter_query='',
+
+                sort_action='custom',
+                sort_mode='multi',
+                sort_by=[]
+            ),
+            style={'height': 750, 'overflowY': 'scroll'},
+            className='six columns'
+        ),
+        html.Div(
+            id='table-paging-with-graph-container',
+            className="five columns"
+        )
+    ]
+)
+
+operators = [['ge ', '>='],
+             ['le ', '<='],
+             ['lt ', '<'],
+             ['gt ', '>'],
+             ['ne ', '!='],
+             ['eq ', '='],
+             ['contains '],
+             ['datestartswith ']]
+
+
+def split_filter_part(filter_part):
+    for operator_type in operators:
+        for operator in operator_type:
+            if operator in filter_part:
+                name_part, value_part = filter_part.split(operator, 1)
+                name = name_part[name_part.find('{') + 1: name_part.rfind('}')]
+
+                value_part = value_part.strip()
+                v0 = value_part[0]
+                if (v0 == value_part[-1] and v0 in ("'", '"', '`')):
+                    value = value_part[1: -1].replace('\\' + v0, v0)
+                else:
+                    try:
+                        value = float(value_part)
+                    except ValueError:
+                        value = value_part
+
+                # word operators need spaces after them in the filter string,
+                # but we don't want these later
+                return name, operator_type[0].strip(), value
+
+    return [None] * 3
+
+
+@app.callback(
+    Output('table-paging-with-graph', "data"),
+    [Input('table-paging-with-graph', "page_current"),
+     Input('table-paging-with-graph', "page_size"),
+     Input('table-paging-with-graph', "sort_by"),
+     Input('table-paging-with-graph', "filter_query")])
+def update_table(page_current, page_size, sort_by, filter):
+    filtering_expressions = filter.split(' && ')
+    dff = df
+    for filter_part in filtering_expressions:
+        col_name, operator, filter_value = split_filter_part(filter_part)
+
+        if operator in ('eq', 'ne', 'lt', 'le', 'gt', 'ge'):
+            # these operators match pandas series operator method names
+            dff = dff.loc[getattr(dff[col_name], operator)(filter_value)]
+        elif operator == 'contains':
+            dff = dff.loc[dff[col_name].str.contains(filter_value)]
+        elif operator == 'datestartswith':
+            # this is a simplification of the front-end filtering logic,
+            # only works with complete fields in standard format
+            dff = dff.loc[dff[col_name].str.startswith(filter_value)]
+
+    if len(sort_by):
+        dff = dff.sort_values(
+            [col['column_id'] for col in sort_by],
+            ascending=[
+                col['direction'] == 'asc'
+                for col in sort_by
+            ],
+            inplace=False
+        )
+
+    return dff.iloc[ 
+        page_current*page_size: (page_current + 1)*page_size
+    ].to_dict('records')
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
